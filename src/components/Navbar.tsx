7b030e1e9a1736d7cea45f90b6e18dd5edea7fe3
anderp15 <100426124+anderp15@users.noreply.github.com>
2026-07-23 14:54:36 -0600

    agregar: barra de navegacion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
+import { useCart } from "@/context/CartContext";
+
 export default function Navbar() {
+  const { usuarioActual, cerrarSesion } = useAuth();
+  const { carrito } = useCart();
+
+  const totalItems = carrito.reduce((sum, p) => sum + p.cantidad, 0);
+
   return (
-    <nav>
-      <h2>Mi Navbar</h2>
+    <nav className = "navbar navbar-expand-lg navbar-dark bg-dark px-3">
+        <Link className="navbar-brand " href="/">Super selectos</Link>
+        <div className="d-flex align-items-center">
+            <link className="btn btn-outline-light me-2 position-relative" href="/cart">
+            Carrito{
+                totalItems > 0 && (<span className="badge bg-danger ms-1">{totalItems}</span>)
+            }
+            </link>
+            {usuarioActual ? (
+                <>
+                <span className="text-white mx-3">Bienvenido al Selectos {usuarioActual.nombre}</span>
+                <button className="btn btn-outline-light" onClick={cerrarSesion}>Cerrar sesion</button>
+                </>
+            ): (
+                <>
+                <link className="btn btn-outline-ligth me-2" href="/login" />Iniciar Sesion
+                <link className="btn btn-outline-ligth" href="/register" /> Registrarse
+                </>
+            )}
+        </div>
     </nav>
-  );
+  )
 }
