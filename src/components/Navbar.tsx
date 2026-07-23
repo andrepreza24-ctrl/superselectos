9d4b5472756b25ff28a886a186257df76c202c36
anderp15 <100426124+anderp15@users.noreply.github.com>
2026-07-23 15:18:50 -0600

    corregir: correcion de rutas y actualizaciones ademas de pruebas en general

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,26 +11,39 @@ export default function Navbar() {
   const totalItems = carrito.reduce((sum, p) => sum + p.cantidad, 0);
 
   return (
-    <nav className = "navbar navbar-expand-lg navbar-dark bg-dark px-3">
-        <Link className="navbar-brand " href="/">Super selectos</Link>
-        <div className="d-flex align-items-center">
-            <link className="btn btn-outline-light me-2 position-relative" href="/cart">
-            Carrito{
-                totalItems > 0 && (<span className="badge bg-danger ms-1">{totalItems}</span>)
-            }
-            </link>
-            {usuarioActual ? (
-                <>
-                <span className="text-white mx-3">Bienvenido al Selectos {usuarioActual.nombre}</span>
-                <button className="btn btn-outline-light" onClick={cerrarSesion}>Cerrar sesion</button>
-                </>
-            ): (
-                <>
-                <link className="btn btn-outline-ligth me-2" href="/login" />Iniciar Sesion
-                <link className="btn btn-outline-ligth" href="/register" /> Registrarse
-                </>
-            )}
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <Link className="navbar-brand" href="/">
+        Super selectos
+      </Link>
+      
+      <div className="d-flex align-items-center">
+        <Link className="btn btn-outline-light me-2 position-relative" href="/cart">
+          Carrito
+          {totalItems > 0 && (
+            <span className="badge bg-danger ms-1">{totalItems}</span>
+          )}
+        </Link>
+
+        {usuarioActual ? (
+          <>
+            <span className="text-white mx-3">
+              Bienvenido al Selectos {usuarioActual.nombre}
+            </span>
+            <button className="btn btn-outline-light" onClick={cerrarSesion}>
+              Cerrar sesión
+            </button>
+          </>
+        ) : (
+          <>
+            <Link className="btn btn-outline-light me-2" href="/login">
+              Iniciar Sesión
+            </Link>
+            <Link className="btn btn-outline-light" href="/register">
+              Registrarse
+            </Link>
+          </>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
