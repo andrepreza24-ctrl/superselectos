acefd522fe52803a23ce6fea3cbf1c287c6b8e01
Andre Preza <andrepreza24@gmail.com>
2026-07-24 22:40:56 -0600

    mejorar: diseño de pagina de redireccionamiento

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -5,7 +5,6 @@ export default function checkoutSuccessPage(){{
         <div className="text-center py-5">
             <h2 className="text-success mb-3">La compra fue realizada con exito</h2>
             <p>Tu factura fue enviada a tu correo electronico</p>
-            <link href="/" className="btm btn-primary mt-3" />
             <Link href="/" className="btn btn-primary mt-3">Volver al catalogo</Link>
         </div>
     );
